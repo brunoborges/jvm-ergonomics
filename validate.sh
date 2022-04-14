595768248c7aeb8cf4570c6094a021c1038b9833
Bruno Borges <bruno.borges@gmail.com>
2022-04-14 18:25:19 +0000

    Script

---
+++ validate.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+imagerepo=$1
+imageversion=$2
+
+containerimage=$1:$2
+
+# Check if Microsoft Build of OpenJDK
+if [[ $image == *"microsoft"* ]]; then
+  containerimage = $containerimage-ubuntu
+fi
+
+echo "Running image $containerimage"
+        docker run \ 
+          --memory=256m \
+          $containerimage \
+          java -XX:+PrintFlagsFinal -XX:+UseContainerSupport -version | grep 'SerialGC'
