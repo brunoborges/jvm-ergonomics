1bfa38091f9374f2b1dfb54ec7885bfa2cf0dea9
Bruno Borges <bruno.borges@gmail.com>
2022-04-14 18:36:09 +0000

    Better check and error message

--- validate.sh
+++ validate.sh
@@ -5,10 +5,18 @@ imageversion=$2
 containerimage=$1:$2
 
 # Check if Microsoft Build of OpenJDK
-if [[ $containerimage == *"microsoft"* ]]; then
+if [[ $imagerepo == "mcr.microsoft.com/openjdk/jdk" ]]; then
   containerimage = $containerimage-ubuntu
 fi
 
-echo "Running image $containerimage"
+echo "Running image $containerimage with 1791mb of memory..."
 
-docker run --memory=256m $containerimage java -XX:+PrintFlagsFinal -XX:+UseContainerSupport -version | grep 'SerialGC'
+docker run --memory=1791m $containerimage java -XX:+PrintFlagsFinal -XX:+UseContainerSupport -version > output
+
+grep '.*SerialGC.*true.*' output
+
+if [[ $? == 0 ]]; then
+  echo "PASSED: SerialGC is selected"
+else
+  echo "FAILED: SerialGC is not selected"
+fi  
