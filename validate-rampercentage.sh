c5b1a9b93d53b1dd81fa2211151b8ef343a4304a
Bruno Borges <brborges@microsoft.com>
2022-09-08 15:00:35 +0200

    Check RAMPercentage

---
+++ validate-rampercentage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+imagerepo=$1
+imageversion=$2
+
+containerimage=$1:$2
+
+# Check if Microsoft Build of OpenJDK
+if [[ $imagerepo == "mcr.microsoft.com/openjdk/jdk" ]]; then
+  containerimage=$containerimage-ubuntu
+fi
+
+echo "Pull container image ${containerimage}..."
+docker pull $containerimage
+
+echo "Running image $containerimage ..."
+
+# Test MaxRAMPercentage
+echo "-- Test for MaxRAMPercentage..."
+docker run --memory=128 $containerimage java -XX:+PrintFlagsFinal -version > output
+
+grep 'RAMPercentage' output
+
+cat output
